14421faa945ed11a6b5ba652d72af6ff28d96247
Raunak Shrestha <sraunak@gmail.com>
2020-04-28 19:14:14 -0700

    update 2020-04-28

--- R/plot_tests_labs_line.R
+++ R/plot_tests_labs_line.R
@@ -9,20 +9,23 @@ p1 <- ggplot(d_nphl, aes(x=date, y=total_test)) +
           axis.text.y = element_text(size = 15, color="#000000"),
           axis.title = element_text(size = 15, color="#000000"),
           plot.title = element_text(size = 15, color="#000000", hjust=0.5),
-          panel.grid.major.y = element_line(size=0.5, color="#BDBDBD"),
-          panel.grid.minor.y = element_line(size=0.25, color="#BDBDBD"),
+          panel.grid.major.y = element_line(size=0.5, color="#FFFFFF"),
+          panel.grid.minor.y = element_line(size=0.25, color="#FFFFFF"),
           panel.grid.major.x = element_blank(),
           panel.grid.minor.x = element_blank(),
-          axis.ticks = element_line(size=0.4, color="#000000"), 
+          axis.ticks = element_line(size=0.4, color="#FFFFFF"), 
           strip.text = element_text(size=10, color="#000000"),
-          strip.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
-          panel.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
+          strip.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+          panel.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+          plot.background = element_rect(fill = "#d5e4eb"),
           axis.line.x = element_line(size = 1.5, color="#000000"),
           axis.line.y = element_blank(),
-          legend.text = element_text(size = 10, color="#000000"),
+          legend.background = element_rect(fill="#d5e4eb"),
+          legend.text = element_text(size = 12, color="#000000"),
           legend.title = element_blank(),
-          legend.key.size = unit(0.5, "cm"),
-          legend.position = "bottom") +
+          legend.key = element_rect(fill="#d5e4eb"),
+          legend.key.size = unit(0.8, "cm"),
+          legend.position = "right") +
         ylab("Total RT-PCR Test Conducted") +
         xlab("Date") + 
         ggtitle("") 
@@ -36,24 +39,27 @@ p2 <- ggplot(d_labs, aes(x=date, y=total_test)) +
       geom_point(aes(color=lab_id), alpha=1, size=2) +
       geom_line(aes(color=lab_id), alpha=1, size=1)+
       theme(
-        axis.text.x = element_text(size = 15, color="#000000"),
-        axis.text.y = element_text(size = 15, color="#000000"),
-        axis.title = element_text(size = 15, color="#000000"),
-        plot.title = element_text(size = 15, color="#000000", hjust=0.5),
-        panel.grid.major.y = element_line(size=0.5, color="#BDBDBD"),
-        panel.grid.minor.y = element_line(size=0.25, color="#BDBDBD"),
-        panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        axis.ticks = element_line(size=0.4, color="#000000"), 
-        strip.text = element_text(size=10, color="#000000"),
-        strip.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
-        panel.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
-        axis.line.x = element_line(size = 1.5, color="#000000"),
-        axis.line.y = element_blank(),
-        legend.text = element_text(size = 10, color="#000000"),
-        legend.title = element_blank(),
-        legend.key.size = unit(0.5, "cm"),
-        legend.position = "bottom") +
+          axis.text.x = element_text(size = 15, color="#000000"),
+          axis.text.y = element_text(size = 15, color="#000000"),
+          axis.title = element_text(size = 15, color="#000000"),
+          plot.title = element_text(size = 15, color="#000000", hjust=0.5),
+          panel.grid.major.y = element_line(size=0.5, color="#FFFFFF"),
+          panel.grid.minor.y = element_line(size=0.25, color="#FFFFFF"),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          axis.ticks = element_line(size=0.4, color="#FFFFFF"), 
+          strip.text = element_text(size=10, color="#000000"),
+          strip.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+          panel.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+          plot.background = element_rect(fill = "#d5e4eb"),
+          axis.line.x = element_line(size = 1.5, color="#000000"),
+          axis.line.y = element_blank(),
+          legend.background = element_rect(fill="#d5e4eb"),
+          legend.text = element_text(size = 12, color="#000000"),
+          legend.title = element_blank(),
+          legend.key = element_rect(fill="#d5e4eb"),
+          legend.key.size = unit(0.8, "cm"),
+          legend.position = "right") +
       ylab("Total RT-PCR Test Conducted") +
       xlab("Date") + 
       ggtitle("") 
--- R/plot_total_tests_line.R
+++ R/plot_total_tests_line.R
@@ -2,24 +2,27 @@ p <- ggplot(dat_total, aes(x=date, y=total_test)) +
         geom_point(color=test_color, alpha=1, size=2) +
         geom_line(color=test_color, alpha=1, size=1)+
         theme(
-          axis.text.x = element_text(size = 15, color="#000000"),
-          axis.text.y = element_text(size = 15, color="#000000"),
-          axis.title = element_text(size = 15, color="#000000"),
-          plot.title = element_text(size = 15, color="#000000", hjust=0.5),
-          panel.grid.major.y = element_line(size=0.5, color="#BDBDBD"),
-          panel.grid.minor.y = element_line(size=0.25, color="#BDBDBD"),
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          axis.ticks = element_line(size=0.4, color="#000000"), 
-          strip.text = element_text(size=10, color="#000000"),
-          strip.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
-          panel.background = element_rect(fill="#FFFFFF", color="#FFFFFF"),
-          axis.line.x = element_line(size = 1.5, color="#000000"),
-          axis.line.y = element_blank(),
-          legend.text = element_text(size = 10, color="#000000"),
-          legend.title = element_blank(),
-          legend.key.size = unit(0.5, "cm"),
-          legend.position = "none") +
+                axis.text.x = element_text(size = 15, color="#000000"),
+                axis.text.y = element_text(size = 15, color="#000000"),
+                axis.title = element_text(size = 15, color="#000000"),
+                plot.title = element_text(size = 15, color="#000000", hjust=0.5),
+                panel.grid.major.y = element_line(size=0.5, color="#FFFFFF"),
+                panel.grid.minor.y = element_line(size=0.25, color="#FFFFFF"),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank(),
+                axis.ticks = element_line(size=0.4, color="#FFFFFF"), 
+                strip.text = element_text(size=10, color="#000000"),
+                strip.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+                panel.background = element_rect(fill="#d5e4eb", color="#d5e4eb"),
+                plot.background = element_rect(fill = "#d5e4eb"),
+                axis.line.x = element_line(size = 1.5, color="#000000"),
+                axis.line.y = element_blank(),
+                legend.background = element_rect(fill="#d5e4eb"),
+                legend.text = element_text(size = 12, color="#000000"),
+                legend.title = element_blank(),
+                legend.key = element_rect(fill="#d5e4eb"),
+                legend.key.size = unit(0.8, "cm"),
+                legend.position = "none") +
         ylab("Total RT-PCR Test Conducted") +
         xlab("Date") + 
         ggtitle("") 
